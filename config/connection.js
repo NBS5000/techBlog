9cbaeb15dfc793e08a3cb346a0ddee4b496355da
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-02 15:20:36 +1000

    asset file renamed to public css added

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,6 @@
-// require('dotenv').config();
-
-// const Sequelize = require('sequelize');
-
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
-
-// module.exports = sequelize;
-
-const Sequelize = require("sequelize");
 require("dotenv").config();
+const Sequelize = require("sequelize");
+
 let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -23,11 +8,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: "localhost",
       dialect: "mysql",
-      port: 3306,
+      port: 3305,
     }
   );
 }
